a396ec22cef66afbadde95c4cfad2a70d2ef47c7
Antonio <antonio.lucas@trt4.jus.br>
2020-11-07 20:38:24 -0300

    [#78] Convert 'cm' to 'pt'

--- index.js
+++ index.js
@@ -671,7 +671,7 @@ module.exports = function(htmlText, options) {
   }
 
   /**
-   * Convert 'px'/'rem' to 'pt', and return false for the other ones. If it's only a number, it will just return it
+   * Convert 'px'/'rem'/'cm' to 'pt', and return false for the other ones. If it's only a number, it will just return it
    *
    * @param  {String} val The value with units (e.g. 12px)
    * @return {Number|Boolean} Return the pt value, or false
@@ -679,7 +679,7 @@ module.exports = function(htmlText, options) {
   var convertToUnit = function(val) {
     // if it's just a number, then return it
     if (!isNaN(parseFloat(val)) && isFinite(val)) return val*1;
-    var mtch = (val+"").trim().match(/^(\d+(\.\d+)?)(pt|px|rem)$/);
+    var mtch = (val+"").trim().match(/^(\d+(\.\d+)?)(pt|px|rem|cm)$/);
     // if we don't have a number with supported units, then return false
     if (!mtch) return false;
     val = mtch[1];
@@ -690,6 +690,10 @@ module.exports = function(htmlText, options) {
       }
       case 'rem':{
         val *= 12; // default font-size is 12pt
+        break;
+      }
+      case 'cm':{
+        val = Math.round(val * 28.34646); // 1cm => 28.34646
         break;
       }
     }
