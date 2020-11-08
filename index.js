db8ed55e127e749142b7ebeb52b5f8bb65171349
Antonio <antonio.lucas@trt4.jus.br>
2020-11-08 00:13:39 -0300

    [#77] allow the user to extend defaultStyles

--- index.js
+++ index.js
@@ -79,6 +79,11 @@ module.exports = function(htmlText, options) {
             else defaultStyles[keyStyle][k] = options.defaultStyles[keyStyle][k];
           }
         }
+      } else {
+        defaultStyles[keyStyle] = {}
+        for (var k in options.defaultStyles[keyStyle]) {
+          defaultStyles[keyStyle][k] = options.defaultStyles[keyStyle][k];
+        }
       }
     }
   }
