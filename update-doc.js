27e13328ba4a41d433f3de9d76d8b7e39af47ef9
Aymeric <Aymkdn@users.noreply.github.com>
2021-10-19 10:01:41 +0200

    update doc

--- update-doc.js
+++ update-doc.js
@@ -2,8 +2,16 @@ const package = require("./package.json");
 const fs = require("fs");
 const fileName = "./docs/index.html";
 
+// delete all browser-*.js files
+let content = fs.readdirSync('./docs/');
+content.forEach(function(file) {
+  if (/^browser-[\d\.]+\.js$/.test(file)) fs.unlinkSync('./docs/'+file);
+});
+// copy the new one
+fs.copyFileSync('./browser.js', './docs/browser-'+package.version+'.js');
+// update the html file
 fs.readFile(fileName, 'utf8', function(err, content) {
-  content = content.replace(/html-to-pdfmake@[\d\.]+\/browser\.js/, 'html-to-pdfmake@'+package.version+'/browser.js');
+  content = content.replace(/browser-[\d\.]+\.js/, 'browser-'+package.version+'.js');
   fs.writeFile(fileName, content, function(err) {
     if (err) throw err;
     console.log("Documentation updated with last version");
